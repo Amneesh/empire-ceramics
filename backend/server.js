cd6716bbc22ae5c9364e2a3d8b4eff4be9de71a5
Amneesh <amneeshsingh5@gmail.com>
2024-10-21 23:48:51 -0700

    CHANGES-LIVE

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,18 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/fireba
 import { getFirestore, addDoc, collection, doc, updateDoc, deleteDoc, getDocs, where } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 import { getDatabase, set, get, child, query } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";   // ref
 // Initialize Firestore
-
+import dotenv from 'dotenv'
+dotenv.config();
+console.log(process.env.FIREBASE_API_KEY);
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+//   };
 
 
 const firebaseConfig = {
